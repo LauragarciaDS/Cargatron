f5a7b9f30f385eaecb1d1e5238256e50065c502a
anonimaCode <93741624+anonimacode@users.noreply.github.com>
2022-05-26 17:34:23 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Cargaton
+# Cargatron
+#### Laura García Sánchez
+
+App realizada en el bootcamp de **Data Science** en The Bridge.
+
+>>Su objetivo es encontrar el cargador más cercano.
---
+++ main.py
@@ -0,0 +1,43 @@
+from importlib.resources import path
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import funtions as ft
+
+
+#st.set_page_config(layout='wide')
+
+
+
+st.set_page_config(layout='wide')
+
+st.markdown('**Como te encuentras hoy?**')
+
+pagina = st.sidebar.selectbox('Página', ('Home', 'Datos'))
+
+if pagina == 'Home':
+    ft.home()
+elif pagina == 'Datos':  # tantas paginas como tengamos
+    ft.datos()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+# with st.expander("Descripcion"):  a mi me ha salido con esto
+#     st.markdown("Esto será una descripción para encontrar elcargador más cercano a tu casa")
+#     st.code("""with st.expander("Descripcion"):
+#     st.markdown("Esto será una descripción para encontrar elcargador más cercano a tu casa")""")
+
+
+
+
